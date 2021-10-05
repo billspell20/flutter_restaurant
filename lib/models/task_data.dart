8694e0f4d332e4fa955f138b5df7db224ceb8d18
Bill Spell <william.spell@vanderbilt.edu>
2021-10-05 00:30:19 -0500

    post req add

--- lib/models/task_data.dart
+++ lib/models/task_data.dart
@@ -3,7 +3,24 @@ import 'package:restaurant_flutter/models/task.dart';
 import 'dart:collection';
 import 'package:http/http.dart' as http;
 
-var createurl = Uri.parse('https://www.restaurant-list.com/todos/add/');
+var createurl =
+    Uri.parse('https://www.restaurant-list.com/www.restaurant-list.com');
+sendRequest() async {
+  Map data = {
+    'todo_description': 'fluttertest',
+    'todo_priority': '',
+    'todo_completed': 'false',
+    'user_id': "null"
+  };
+
+  var url = 'https://www.restaurant-list.com/todos/add/';
+  http
+      .post(Uri.https('www.restaurant-list.com', '/todos/add/'), body: data)
+      .then((response) {
+    print("Response status: ${response.statusCode}");
+    print("Response body: ${response.body}");
+  });
+}
 
 //todo create nodejs post and get requests here
 class TaskData extends ChangeNotifier {
@@ -20,17 +37,10 @@ class TaskData extends ChangeNotifier {
     return _tasks.length;
   }
 
-  Future<void> addTask(String newTaskTitle, String newTaskPriority) async {
+  void addTask(String newTaskTitle, String newTaskPriority) {
     final task = Task(name: newTaskTitle, priority: newTaskPriority);
     _tasks.add(task);
-    var response = await http.post(createurl, body: {
-      'todo_description': 'flutter test',
-      'todo_priority': '',
-      'todo_completed': false,
-      'user_id': "null"
-    });
-    print('Response status: ${response.statusCode}');
-    print('Response body: ${response.body}');
+    sendRequest();
     notifyListeners();
   }
 
