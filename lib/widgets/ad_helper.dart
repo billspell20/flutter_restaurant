dbecc8116c4abe1adf5d1da72ca2b9b95147d6cd
Bill Spell <william.spell@vanderbilt.edu>
2021-10-21 22:01:39 -0500

    versioning

--- lib/widgets/ad_helper.dart
+++ lib/widgets/ad_helper.dart
@@ -3,9 +3,9 @@ import 'dart:io';
 class AdHelper {
   static String get bannerAdUnitId {
     if (Platform.isAndroid) {
-      return 'ca-app-pub-3940256099942544/6300978111';
+      return 'ca-app-pub-1590976018869330/9422676485';
     } else if (Platform.isIOS) {
-      return 'ca-app-pub-3940256099942544/2934735716';
+      return 'ca-app-pub-1590976018869330/5298409278';
     } else {
       throw new UnsupportedError('Unsupported platform');
     }
